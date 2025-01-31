b4865456ab5ef80c7c763135e674944b9331c08f
Mill Daulagala <mill.daulagala@gmail.com>
2025-01-31 21:40:12 +1100

    added maven dependencies

---
+++ src/main/java/com/library/knowledge_hub_events_producer/KnowledgeHubEventsProducerApplication.java
@@ -0,0 +1,13 @@
+package com.library.knowledge_hub_events_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeHubEventsProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeHubEventsProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/library/knowledge_hub_events_producer/KnowledgeHubEventsProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.knowledge_hub_events_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeHubEventsProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
